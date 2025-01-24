bf2779d34c1bf11cac7ae0baa51ac4dfb574486a
Mukul Sharma <mukul02015@gmail.com>
2025-01-24 23:07:53 +0530

    theme toggle logic, add theme to contact form

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,76 @@
 /*!
-    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
+ * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+ */
 
-$('.flip-container .flipper').click(function() {
-    $(this).closest('.flip-container').toggleClass('hover');
-    $(this).css('transform, rotateY(180deg)');
-});
+// const buttons = document.querySelectorAll('.mode-toggle')
 
-function toggleTheme(){
+// buttons.forEach(button => {
+//   button.addEventListener('click', function () {
+//     document.body.classList.toggle('light-mode')
+//     document.querySelectorAll('.fa-sun').forEach(icon => icon.classList.toggle('fa-moon'))
+
+//     if (document.body.classList.contains('light-mode')) {
+//       localStorage.setItem('lightMode', 'enabled')
+//     } else {
+//       localStorage.setItem('lightMode', 'disabled')
+//     }
+//   })
+// })
+
+// if (localStorage.getItem('lightMode') === 'enabled') {
+//   document.body.classList.add('light-mode')
+//   document.querySelectorAll('.fa-sun').forEach(icon => icon.classList.add('fa-moon'))
+// }
+
+// $(".flip-container .flipper").click(function () {
+//   $(this).closest(".flip-container").toggleClass("hover");
+//   $(this).css("transform, rotateY(180deg)");
+// });
+// localStorage.setItem("darkMode", "disabled")
+function toggleTheme() {
+  var element = document.body;
+  localStorage.setItem("darkMode", (localStorage.getItem("darkMode") === "enabled") ? "disabled" : "enabled");
+  document.getElementById("theme-check-box").checked = (localStorage.getItem("darkMode") === "enabled") ? true : false;
+  element.classList.toggle("dark-mode");
+}
+
+function checkLightMode() {
+  console.log(localStorage.getItem("darkMode"));
+  if (localStorage.getItem("darkMode") === "enabled") {
+    console.log('check enable')
+    document.getElementById("theme-check-box").checked = true
     var element = document.body;
     element.classList.toggle("dark-mode");
+  } else {
+    console.log('check disable')
+    localStorage.setItem("darkMode", "disabled");
+  }
 }
 
-function flipCard(){
-    var element = document.body;
-    element.classList.toggle("card-container:active");
+function flipCard() {
+  var element = document.body;
+  element.classList.toggle("card-container:active");
 }
-function validateForm (){
-
-    fetch('https://api.ipify.org/?format=json')
-    .then(results => results.json())
-    .then(data => sIp = data.ip)
+function validateForm() {
+  fetch("https://api.ipify.org/?format=json")
+    .then((results) => results.json())
+    .then((data) => (sIp = data.ip))
     .then(() => console.log(sIp));
-    
-    var subject = document.forms["cForm"]["subject"].value;
-    subject = subject.replace(/\s+/g, '');
-    var msg = document.forms["cForm"]["text"].value;
-    var alphaRegex = /^[A-Za-z]+$/;
-    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    if (!subject.match(alphaRegex)){
-        alert("Subject field can only have alphabets! Refresh and try again.");
-        return false;
-    }
-    if (msg.match(urlRegex) != null) {
-        alert("No links allowed! Refresh and try again.");
-        return false;
-    }
 
+  var subject = document.forms["cForm"]["subject"].value;
+  subject = subject.replace(/\s+/g, "");
+  var msg = document.forms["cForm"]["text"].value;
+  var alphaRegex = /^[A-Za-z]+$/;
+  var urlRegex =
+    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+  if (!subject.match(alphaRegex)) {
+    alert("Subject field can only have alphabets! Refresh and try again.");
+    return false;
+  }
+  if (msg.match(urlRegex) != null) {
+    alert("No links allowed! Refresh and try again.");
+    return false;
+  }
 }
